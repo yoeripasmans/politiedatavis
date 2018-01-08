d6d0e6410232fd2e6cbe282c02c82f2def685546
Leon van Zijl <leonvanzijlprive@gmail.com>
2018-01-08 17:41:34 +0100

    De kerstboom kleuren weer terug

--- src/index.js
+++ src/index.js
@@ -36,11 +36,11 @@ d3.tsv("data/data.tsv", function(error, data) {
         // .attr("fill", "blue");
 		.attr("fill", function(d) {
 			if (d.status == "Normaal") {
-		        return "#416066";
+		        return "#19C5E5";
 		    } else if (d.status == "Aangepast") {
-		        return "#0e8196";
+		        return "#FFA101";
 		    } else if (d.status == "Verwijderd") {
-		        return "#19C5E5#416066";
+		        return "#FF5E5B";
 		    } else if (d.status == "Inactief") {
 		        return "#e2e2e2";
 		    }
