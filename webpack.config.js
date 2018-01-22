2eb66db7eacf99b57cb620de054098702a463c9a
Yoeri Pasmans <yoerip@live.nl>
2018-01-22 22:02:02 +0100

    Revert "Changed the build"
    This reverts commit 6e582c207a77cabb4bb7591da19732dde6858b71.

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,64 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
 
-	module: {
-		rules: [
-			// Add babel-loader to compile ES6 to ES5
-			{
-				test: /\.js$/,
-				exclude: [/node_modules/],
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: ['es2015', 'stage-0']
-					},
-				}],
-			},
-			// Add sass-loader to compile scss to css
-			{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract([
-					'css-loader', 'sass-loader'
-				]),
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]',
-					outputPath: 'assets/images/'
-				}
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]',
-					outputPath: 'assets/fonts/'
-				}
-			},
-			{
-				test: /\.(csv|tsv)$/,
-				use: [
-					'csv-loader'
-				]
-			},
-		]
-	},
-	devtool: 'inline-source-map',
-	plugins: [
-		new ExtractTextPlugin({
-			filename: 'bundle.css',
-			allChunks: true,
-		}),
+  module: {
+    rules: [
+      // Add babel-loader to compile ES6 to ES5
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-0']
+          },
+        }],
+      },
+      // Add sass-loader to compile scss to css
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract([
+          'css-loader', 'sass-loader'
+        ]),
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader'
+         ]
+       }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'bundle.css',
+      allChunks: true,
+    })
 
-		new HtmlWebpackPlugin({
-			title: 'Output Management'
-		}),
+  ],
 
-		new CopyWebpackPlugin([{
-				from: 'src/assets/images/frames',
-				to: 'assets/images/frames'
-			},
-			{
-				from: 'src/data',
-				to: 'data'
-			},
-		]),
-
-	],
-
-	// Automatically reload the page when compilation is done.
-	devServer: {
-		port: 3000,
-		compress: true,
-		contentBase: 'dist/',
-	},
+  // Automatically reload the page when compilation is done.
+  devServer: {
+    port: 3000,
+    compress: true,
+    contentBase: 'dist/',
+  },
 
 };
