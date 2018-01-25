fbda6841ab893488cb15cb56911d289c5fd89fe9
Leon van Zijl <leonvanzijlprive@gmail.com>
2018-01-25 12:54:02 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ var svg = d3.select("body").append("div").attr('class', 'svg-container')
 	.append("g");
 
 var simulation = d3.forceSimulation()
-	.force("r", d3.forceRadial(10).strength(0.005)) //This force makes sure every circle is in a radius
+	.force("r", d3.forceRadial(10)) //This force makes sure every circle is in a radius
 	.force("collide", d3.forceCollide(function(d) {
 		if (window.innerWidth < 500) {
 			if (d.status == "Inactief") {
@@ -39,15 +39,9 @@ var simulation = d3.forceSimulation()
 		} else {
 			return circleSize(d.totaleSchendingen) + 2;
 		}
-<<<<<<< HEAD
-	}));
-	// .alpha(0.02)
-	// .alphaDecay(0.01);
-=======
 	}))
 	.alpha(0.02)
 	.alphaDecay(0.01);
->>>>>>> laurens-develop
 
 // Prep the tooltip bits, initial display is hidden
 //Append tool-tip to conainter
@@ -454,7 +448,7 @@ d3.tsv("data/data.tsv", function(error, data) {
 					simulation
 						.force("r", d3.forceRadial(function(d) {
 							return 0;
-						}).strength(0.005))
+						}))
 						.force("collide", d3.forceCollide(function(d) {
 							if (window.innerWidth < 500) {
 								if (d.status == "Inactief") {
@@ -466,11 +460,7 @@ d3.tsv("data/data.tsv", function(error, data) {
 								return circleSize(d.totaleSchendingen) + 2;
 							}
 						}))
-<<<<<<< HEAD
-						.alpha(0.2)
-=======
 						.alpha(0.15)
->>>>>>> laurens-develop
 						.alphaDecay(0.015) //Makes sure the alpha doesn't decay too quickly so the clicked circle gets to the middle
 						.restart();
 
